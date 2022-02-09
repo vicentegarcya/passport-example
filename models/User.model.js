1aef93311574507361984cd70347448f8e2ebf0e
Pablo Laso <plasocortabitarte@gmail.com>
2022-02-09 11:47:19 +0100

    basic user model

---
+++ models/User.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const PASSWORD_PATTERN = /^.{8,}$/i;
+const SALT_ROUNDS = 10;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: 'name is required',
+    minLength: [3, 'name needs at least 3 chars']
+  },
+  email: {
+    type: String,
+    required: 'email is required',
+    match: [EMAIL_PATTERN, 'email is not valid'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: 'password is required',
+    match: [PASSWORD_PATTERN, 'password needs at least 8 chars'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
